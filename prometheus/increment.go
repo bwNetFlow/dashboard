057c54952427e5d4349e56427984bfc2e02d25db
christopher <post@c-ha.de>
2018-09-04 14:09:33 +0200

    simplify prometheus labels for testing

--- prometheus/increment.go
+++ prometheus/increment.go
@@ -10,13 +10,13 @@ import (
 // Increment updates the counters by one flow
 func (exporter *Exporter) Increment(flow *flow.FlowMessage) {
 	labels := prometheus.Labels{
-		"src_port":  fmt.Sprint(flow.GetSrcPort()),
-		"dst_port":  fmt.Sprint(flow.GetDstPort()),
-		"proto":     fmt.Sprint(flow.GetProto()),
-		"src_if":    fmt.Sprint(flow.GetSrcIf()),
-		"dst_if":    fmt.Sprint(flow.GetDstIf()),
-		"direction": fmt.Sprint(flow.GetDirection()),
-		"cid":       fmt.Sprint(flow.GetCid()),
+		// "src_port":  fmt.Sprint(flow.GetSrcPort()),
+		// "dst_port":  fmt.Sprint(flow.GetDstPort()),
+		// "proto":     fmt.Sprint(flow.GetProto()),
+		// "src_if": fmt.Sprint(flow.GetSrcIf()),
+		// "dst_if": fmt.Sprint(flow.GetDstIf()),
+		// "direction": fmt.Sprint(flow.GetDirection()),
+		"cid": fmt.Sprint(flow.GetCid()),
 	}
 
 	flowNumber.With(labels).Inc()
