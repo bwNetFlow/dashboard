fbe59298b2e66969aa97f9400d482b54ab23df6e
christopher <post@c-ha.de>
2019-01-22 13:55:09 +0100

    finish influx connector to push into cid DBs

--- connectors/influx/connector.go
+++ connectors/influx/connector.go
@@ -54,25 +54,36 @@ func (connector *Connector) startPushCycle() {
 }
 
 func (connector *Connector) push() {
-	fmt.Print("Push stuff to Influx")
+	connector.pushCounter(counters.Msgcount)
+	connector.pushCounter(counters.KafkaOffsets)
+	connector.pushCounter(counters.FlowNumber)
+	connector.pushCounter(counters.FlowBytes)
+	connector.pushCounter(counters.FlowPackets)
+	connector.pushCounter(counters.HostBytes)
+	connector.pushCounter(counters.HostConnections)
+}
 
+func (connector *Connector) pushCounter(counter counters.Counter) {
+	for cid := range counter.CustomerIndex {
+		counter.Access.Lock()
+		connector.pushCounterCustomer(counter, cid)
+		counter.Access.Unlock()
+	}
+}
+
+func (connector *Connector) pushCounterCustomer(counter counters.Counter, cid string) {
 	// Create a new point batch
 	bp, err := client.NewBatchPoints(client.BatchPointsConfig{
-		Database:  "bwnetflow",
+		Database:  "bwnetflow-" + cid,
 		Precision: "s",
 	})
 	if err != nil {
 		fmt.Println("Error creating NewBatchPoints: ", err.Error())
 	}
 
-	// Transform counter values to influx points
-	pts := transformCounter(counters.Msgcount)
-	bp.AddPoints(pts)
-	pts = transformCounter(counters.FlowNumber)
-	bp.AddPoints(pts)
-	pts = transformCounter(counters.FlowBytes)
-	bp.AddPoints(pts)
-	pts = transformCounter(counters.FlowPackets)
+	// get measurements as points
+	hashes := counter.CustomerIndex[cid]
+	pts := transformCounter(counter, hashes)
 	bp.AddPoints(pts)
 
 	// Write the batch
@@ -80,4 +91,27 @@ func (connector *Connector) push() {
 	if err != nil {
 		fmt.Println("Error writing to influx: ", err.Error())
 	}
+}
+
+func transformCounter(counter counters.Counter, hashes []uint32) []*client.Point {
+	pts := make([]*client.Point, 0)
+	for _, hash := range hashes {
+		items := counter.Fields[hash]
+		labels := items.Label
+		val := items.Value
+
+		tags := map[string]string{}
+		for k, v := range labels.Fields {
+			tags[k] = v
+		}
+		fields := map[string]interface{}{
+			"count": int64(val),
+		}
+		pt, err := client.NewPoint(counter.Name, tags, fields, time.Now())
+		if err != nil {
+			fmt.Println("Error: ", err.Error())
+		}
+		pts = append(pts, pt)
+	}
+	return pts
 }
